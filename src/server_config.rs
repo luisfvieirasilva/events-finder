2be00c6a7d98d7e53a1925ed2136cbf442ba6936
Luis Silva <luisfvieirasilva@gmail.com>
2023-12-28 13:04:57 -0300

    feat: add /health endpoint
    Also adds ServerConfig struct that reads from config file

---
+++ src/server_config.rs
@@ -0,0 +1,23 @@
+use config::{Config, ConfigError, Environment, File};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct ServerConfig {
+    pub address: String,
+    pub port: u16,
+}
+
+impl ServerConfig {
+    pub fn load(config_file: &str) -> Result<ServerConfig, ConfigError> {
+        let builder = Config::builder()
+            .set_default("address", "127.0.0.1")?
+            .set_default("port", 8080)?
+            .add_source(File::with_name(config_file))
+            .add_source(Environment::with_prefix("APP"));
+
+        let settings = builder.build()?;
+
+        // Deserialize the configuration into our struct
+        settings.try_deserialize::<ServerConfig>()
+    }
+}
